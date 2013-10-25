65e0856e6aa41a611a26874584aa38f1de536fd6
Halfdan Rump <halfdan-rump@garage.co.jp>
2013-10-25 13:53:55 +0900

    Initalize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medidata::Application.config.secret_key_base = '64c97a60d639afd221a64d440891a88dcceff21ad2c78fb17b99b9850e1be195c88e3de6909cb57c9907486fa6b48bc22dbca1ff527b35f5c4efea7f10c91a01'
